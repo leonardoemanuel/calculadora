887827faf0106b508f28a1aac176b95f4a9cf3ff
Leonardo Emanuel <leonardodev2005@gmail.com>
2022-03-10 21:50:13 -0300

    mega atualização no site

--- script.js
+++ script.js
@@ -115,10 +115,34 @@ let tabuadaop=''
 /*Raiz Quadrada*/
 let numberRaiz=document.getElementById('txtraiz')
 let raizRes=document.getElementById('raizRes')
-    
     function calcularRaiz(){
         raizRes.innerHTML=`Resultado:${Math.sqrt(numberRaiz.value)}`
     }
+/*Contador*/
+function contador(){
+    let txtinicio=document.getElementById('inicio')
+    let txtpasso=document.getElementById('passo')
+    let txtfim=document.getElementById('fim')
+    
+    let inicio=Number(txtinicio.value)
+    let passo=Number(txtpasso.value)
+    let fim=Number(txtfim.value)
+    
+    let contres=document.getElementById('contres')
+    contres.innerHTML=''
+    contres.innerHTML+=`<li>${inicio}</li>`
+    if(inicio>fim){
+        while(inicio>fim){
+            inicio-=passo
+            contres.innerHTML+=`<li>${inicio}</li>`
+        }
+    }else{
+        while(inicio<fim){
+            inicio+=passo
+            contres.innerHTML+=`<li>${inicio}</li>`
+        }
+    }
+}
 
        
     
